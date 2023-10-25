5de6b65604d8027c9dc675db7ea8e00cbdf9b7b4
TED <teddy.maina@student.moringaschool.com>
2023-10-25 21:40:12 +0300

    completed assignent

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Declare and assign values to variables
+const length = 5; // Length of the rectangle
+const width = 10; // Width of the rectangle
+
+// Calculate the area of the rectangle
+const area = length * width;
+
+// Output the result
+console.log(`The area of the rectangle is: ${area}`);
+
